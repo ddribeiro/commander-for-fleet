a1fbb73ced80f82dbe0d151dd784f2d758f46a67
Dale Ribeiro <ddribeiro@me.com>
2025-02-13 15:28:28 -0500

    reworked policies row

--- App/Policies/AllPoliciesRow.swift
+++ App/Policies/AllPoliciesRow.swift
@@ -9,35 +9,30 @@ import SwiftUI
 
 struct AllPoliciesRow: View {
     @Environment(\.horizontalSizeClass) var sizeClass
-
+    
     var policy: CachedPolicy
-
+    
     var body: some View {
-        HStack {
-            Image(systemName: "checkmark.seal.fill")
-                .font(.system(.title2, design: .rounded))
-                .fontWeight(.semibold)
-                .symbolRenderingMode(.hierarchical)
-                .foregroundStyle(.green)
-#if os(iOS)
-                .frame(width: 40, height: 40)
-#else
-                .frame(width: 20, height: 20)
-#endif
+        VStack(alignment: .leading) {
             Text(policy.wrappedName)
                 .font(.headline)
+                .multilineTextAlignment(.leading)
             if sizeClass == .compact {
-                Spacer()
-
-                VStack {
+                HStack {
                     Text("\(policy.passingHostCount) Passing")
-                        .foregroundStyle(.green)
+                        .foregroundStyle(.white)
+                        .padding(4)
+                        .background(.green, in: RoundedRectangle(cornerRadius: 8))
+                    
+                    
                     Text("\(policy.failingHostCount) Failing")
-                        .foregroundStyle(.red)
+                        .foregroundStyle(.white)
+                        .padding(4)
+                        .background(.red, in: RoundedRectangle(cornerRadius: 8))
                 }
-                .font(.smallCaps(.body)())
+                .font(.subheadline)
             }
+            
         }
-
     }
 }
