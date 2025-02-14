5873821f4eacd7bcbe2dce8cdae9ff0711a63408
Dale Ribeiro <ddribeiro@me.com>
2025-02-14 11:58:37 -0500

    policies row changes

--- App/Policies/AllPoliciesRow.swift
+++ App/Policies/AllPoliciesRow.swift
@@ -13,26 +13,43 @@ struct AllPoliciesRow: View {
     var policy: CachedPolicy
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .center) {
             Text(policy.wrappedName)
                 .font(.headline)
-                .multilineTextAlignment(.leading)
+                .multilineTextAlignment(.center)
             if sizeClass == .compact {
                 HStack {
-                    Text("\(policy.passingHostCount) Passing")
+                    Group {
+                        VStack {
+                            Image(systemName: "checkmark.seal.fill")
+                                .font(.headline)
+                            Text("\(policy.passingHostCount) Passing")
+                        }
+                        .padding(8)
                         .foregroundStyle(.white)
-                        .padding(4)
+                        .frame(width: 120, height: 70)
                         .background(.green, in: RoundedRectangle(cornerRadius: 8))
-                    
-                    
-                    Text("\(policy.failingHostCount) Failing")
+                        
+                        
+                        Spacer()
+                        VStack {
+                            Image(systemName: "xmark.seal.fill")
+                                .font(.headline)
+                            Text("\(policy.failingHostCount) Failing")
+                                
+                        }
                         .foregroundStyle(.white)
-                        .padding(4)
+                        .padding(8)
+                        .frame(width: 120, height: 70)
                         .background(.red, in: RoundedRectangle(cornerRadius: 8))
+                        
+                    }
+                    
                 }
+                
+                .padding()
                 .font(.subheadline)
             }
-            
         }
     }
 }
